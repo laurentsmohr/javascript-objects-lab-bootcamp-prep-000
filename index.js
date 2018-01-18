42d6c2c26571d0b0204beda107cd37d1bffc029f
Laurents Mohr <laurents.mohr@whu.edu>
2018-01-18 15:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-destructivelyDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
